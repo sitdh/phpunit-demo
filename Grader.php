882173fc1926f555e69a09202e5f7a16e9cefd6e
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-09-16 13:54:29 +0700

    Funny grading class

---
+++ Grader.php
@@ -0,0 +1,33 @@
+<?php
+
+class Grader
+{
+    private $score = 0;
+    private $studentId = ''
+
+    public function __construct($studentId, $score)
+    {
+    }
+
+    public function studentId()
+    {
+        return $this->studentId;
+    }
+
+    public function grading()
+    {
+        $gradeLetter = 'F';
+
+        if ($score > 80) {
+            $gradeLatter = 'A';
+        } elseif ($score > 70) {
+            $gradeLatter = 'B';
+        } elseif ($score > 60) {
+            $gradeLatter = 'C';
+        } elseif ($score > 50) {
+            $gradeLatter = 'D';
+        }
+
+        return $gradLetter;
+    }
+}
